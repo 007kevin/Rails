266bcb78d4ed3a32c6d12ff3c961dd5f2d60b364
Kevin <kevinkim1991@gmail.com>
2015-05-10 22:55:16 -0400

    Finish user signup

---
+++ sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_template 'users/new'
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: "",
+                               email: "user@invalid",
+                               password: "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_template 'users/new'
+    assert_difference 'User.count', 1 do
+      # Note the use of the post_via_redirect since the successful user creation in the users_controller
+      # uses the redirect_to.
+      # post_via_redirect arranges to follow the redirect of the controller after successful rendering of the file,
+      # resulting in the rendering of the 'users/show' template
+      post_via_redirect users_path, user: { name: "Example User",
+                                            email: "user@example.com",
+                                            password: "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+  end
+end

de6f797f9939e30efd6038f40d8fd42b698c0a1c
Kevin <kevinkim1991@gmail.com>
2015-05-11 23:58:58 -0400

    Done up to 8.4.5

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user # Listing 8.34
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # flash.new is 
@@ -15,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50} 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,5 +15,29 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                         BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    # BCrypt::Password.new(password_digest) == unencrypted_password 
+    # ^ Is not comparing brypt password digest token directly with a token, which
+    # would imply decrypting the digest in order to compare using ==.
+    # Bcrypt hashing is irreverisble. The comparison operator == is being redefined.
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
